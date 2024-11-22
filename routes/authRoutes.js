21737f69b7cc880c053539400f376c4f343c490e
SilverDragon12 <72190816+SilverDragon12@users.noreply.github.com>
2024-11-22 15:05:29 +0530

    authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const{
-    createFreelancer,createClient,login}=require('../controllers/authController');
+    createFreelancer,createClient,login,logout}=require('../controllers/authController');
 router.post('/register/freelancer',createFreelancer)
 router.post('/register/client',createClient)
 router.post('/login',login)
+router.get('/logout',logout)
 
 module.exports = router;
