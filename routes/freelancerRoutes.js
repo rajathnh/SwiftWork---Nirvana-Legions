862b34d335555711225156622d905607ccb4dc7f
SilverDragon12 <72190816+SilverDragon12@users.noreply.github.com>
2024-11-22 15:06:00 +0530

    Update freelancerRoutes.js

--- routes/freelancerRoutes.js
+++ routes/freelancerRoutes.js
@@ -1,11 +1,13 @@
 const express = require ('express')
 const router = express.Router()
-const {updateFreelancer,
+const {updateFreelancer,getAllFreelancers,
     getFreelancerById,
     deleteFreelancer} = require('../controllers/freelancerController')
-
+const {getSingleFreelancerReview} = require('../controllers/reviewController')
+router.get('/',getAllFreelancers)
 router.get('/:id',getFreelancerById)
 router.patch('/:id',updateFreelancer)
 router.delete('/:id',deleteFreelancer)
 
+router.route('/:id/reviews').get(getSingleFreelancerReview)
 module.exports = router;
