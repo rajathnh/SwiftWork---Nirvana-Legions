aaf85a64fa5f5aba4f3c413a5203c390231401f3
SilverDragon12 <72190816+SilverDragon12@users.noreply.github.com>
2024-11-22 15:03:29 +0530

    authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,19 +1,18 @@
-const CustomError = require('../errors');
-const { isTokenValid } = require('../utils');
+const CustomError = require('../errors')
+const {isTokenValid} = require('../utils')
 
-const authenticateUser = async (req, res, next) => {
+const authenticateUser = async(req,res,next) =>{
   const token = req.signedCookies.token;
-
-  if (!token) {
+  if(!token){
     throw new CustomError.UnauthenticatedError('Authentication Invalid');
   }
-
-  try {
-    const { name, userId, role } = isTokenValid({ token });
-    req.user = { name, userId, role };
+  try{
+    const {name,userId,role} = isTokenValid({token});
+    req.user = {name,userId,role}
     next();
-  } catch (error) {
-    throw new CustomError.UnauthenticatedError('Authentication Invalid');
+
+  }catch(error){
+    throw new CustomError.UnauthenticatedError('Authentication Invalid')
   }
 };
 
