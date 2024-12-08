1047a0caa0c98f71157ec528e80adfb6da0c4a0b
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-12-08 16:47:29 +0530

    FIX: storign reviews

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -4,22 +4,11 @@ const { createReview, getAllReviews, getSingleReview, updateReview, deleteReview
 
 const {authenticateUser} = require('../middleware/authentication'); // Import authentication middleware
 
-// Route setup
-router.post('/', async (req, res) => {
-    try {
-      console.log('Request body:', req.body); // Log the request body to verify the data
-      // Handle the review submission logic here
-    } catch (error) {
-      console.error('Error while submitting review:', error);
-      res.status(500).json({ message: 'Internal Server Error' });
-    }
-  });
-  // Ensure createReview is defined and authenticateUser is used
+
+router.post('/', createReview)
 router.get('/', getAllReviews);
 router.get('/:id', getSingleReview);
 router.patch('/:id', authenticateUser, updateReview);
 router.delete('/:id', authenticateUser, deleteReview);
-//router.get('//:id/reviews', getSingleFreelancerReview);
-
 
 module.exports = router;
