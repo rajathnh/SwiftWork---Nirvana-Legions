98f69fc7e1792ef60ba2d81e978c4908af8ea1a4
Tejas Subramanya K R <tejassujathagnks@gmail.com>
2024-12-10 19:50:29 +0530

    ADD: routes for admin page

--- app.js
+++ app.js
@@ -115,7 +115,8 @@ const routes = [
   { path: '/api/v1/gigs', router: require('./routes/gigRoutes') },
   { path: '/api/v1/proposal', router: require('./routes/proposalRoutes') },
   { path: '/api/v1/chat', router: require('./routes/messageRoutes') },
-  { path: '/api/v1/payment',router:require('./routes/paymentRoutes')}
+  { path: '/api/v1/payment',router:require('./routes/paymentRoutes')},
+  { path: '/api/vi/admin', router:require('./routes/adminRoutes')}
 ];
 
 routes.forEach(route => app.use(route.path, route.router));
