51619397803d26753affd48c22e3625bbef48c7f
SilverDragon12 <rajathnh2@gmail.com>
2025-01-06 17:11:07 +0530

    test

--- public/login.js
+++ public/login.js
@@ -17,7 +17,7 @@ loginForm.addEventListener('submit', async (e) => {
     }
 
     // Send the login request to the server for normal users
-    const response = await fetch('${API_BASE_URL}/api/v1/auth/login', {
+    const response = await fetch(`${API_BASE_URL}/api/v1/auth/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
